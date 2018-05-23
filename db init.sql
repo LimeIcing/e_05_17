c6bda7b715afe5a12a4cf0b614946d0f1beee5d8
Lime Icing <emil577b@stud.kea.dk>
2018-05-23 23:23:42 +0200

    corrected fatal error

--- db init.sql
+++ db init.sql
@@ -29,9 +29,11 @@ CREATE TABLE Booking (
     guestNo INT,
     dateFrom DATE,
     dateTo DATE,
+    roomNo INT,
     PRIMARY KEY(hotelNo, guestNo, dateFrom),
     FOREIGN KEY(hotelNo) REFERENCES Hotel(hotelNo),
-    FOREIGN KEY(guestNo) REFERENCES Guest(guestNo)
+    FOREIGN KEY(guestNo) REFERENCES Guest(guestNo),
+    FOREIGN KEY(roomNo) REFERENCES Room(roomNo)
 );
 
 INSERT INTO Hotel(hotelName, city)
@@ -45,13 +47,20 @@ VALUES	('The Collector', 'Knowhere'),
         ('Groot', 'I am Groot'),
         ('Tony Stark', 'Stark Tower, New York');
 
-INSERT INTO Booking
-VALUES	(2, 1, '2018-04-25', '2018-05-03'),
-		(2, 2, '2018-04-01', '2018-04-25'),
-        (1, 3, '2018-08-05', '2018-08-12'),
-        (3, 4, '2018-05-19', '2018-05-27');
-
 INSERT INTO Room
 VALUES	(1408, 2, 'Single', 0),
 		(232, 2, 'Double', 22),
-		(110, 2, 'Family', 45)
+		(110, 2, 'Family', 45),
+        (422, 2, 'Family', 149),
+        (232, 3, 'Single', 54),
+        (362, 2, 'Double', 105),
+        (430, 2, 'Double', 148),
+        (786, 1, 'Family', 30),
+        (218, 2, 'Single', 104),
+        (900, 3, 'Double', 117);
+
+INSERT INTO Booking
+VALUES	(2, 1, '2018-04-25', '2018-05-03', 110),
+		(2, 2, '2018-04-01', '2018-04-25', 422),
+        (1, 3, '2018-08-05', '2018-08-12', 786),
+        (3, 4, '2018-05-19', '2018-05-27', 900);
